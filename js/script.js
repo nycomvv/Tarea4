73f8fc211e8f2ada629f450cf839ec140c7c6eb8
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-10-26 00:29:30 -0300

    Tarea4

--- js/script.js
+++ js/script.js
@@ -8,9 +8,12 @@ const alumno = {
 
 document.write("<br>");
 document.write("ALUMNO : " + alumno.nombre + " " + alumno.Apellido);
-document.write("<br>");
+document.write("<br><br>");
 //document.write("Notas : " + alumno.Nota);
-document.write("NOTAS : " + alumno.misNotas);
+document.write("NOTAS : N1 : " + alumno.misNotas[0]+ " , N2 : " + alumno.misNotas[1]+
+" , N3 : " + alumno.misNotas[2]+ " , N4 : " + alumno.misNotas[3]+ " , N5 : " +
+ alumno.misNotas[4]+ " , N6 : " + alumno.misNotas[5]);
+
 // Funcion Suma Notas
 function suma (n1, n2, n3, n4, n5, n6) {
     const suma = n1+n2+n3+n4+n5+n6
@@ -29,7 +32,7 @@ document.write("<br>");
 //document.write("Las Notas Son : 6 , 8 , 9 , 2 , 5 , 10");
 document.write("<br>");
 document.write("SUMA: " + ResultadoSuma);
-document.write("<br>");
+document.write("<br><br>");
 document.write("PROMEDIO : " + Promedio);
 
 console.log("Suma de Notas : ", ResultadoSuma)
