7f1068e03aceff512f5ae8752722a6fef449e610
nycomvv <nycomvv@MacBook-Pro-de-MVV.local>
2022-10-27 23:17:56 -0300

    Tarea4

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ const nombre = alumno.nombre;
 
 
 document.write("<br><br>");
-document.write("ALUMNO : " + alumno.nombre + " " + alumno.apellido);
+document.write("<strong>ALUMNO : </strong> " + alumno.nombre + " " + alumno.apellido);
 document.write("<br><br>");
 /*document.write("NOTAS : N1 -> " + alumno.misNotas[0]+ " , N2 -> " + alumno.misNotas[1]+
 " , N3 -> " + alumno.misNotas[2]+ " , N4 -> " + alumno.misNotas[3]+ " , N5 -> " +
  alumno.misNotas[4]+ " , N6 -> " + alumno.misNotas[5]);*/
-document.write("NOTAS : " + alumno.misNotas[0] + " , " + alumno.misNotas[1]+
+document.write("<strong>NOTAS : </strong>" + alumno.misNotas[0] + " , " + alumno.misNotas[1]+
 " , " + alumno.misNotas[2]+ " , " + alumno.misNotas[3]+ " , " +
  alumno.misNotas[4]+ " , " + alumno.misNotas[5]);
 
